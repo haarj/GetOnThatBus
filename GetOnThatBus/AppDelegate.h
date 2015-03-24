ce474b743eeb331ada97b8d2ebfa7a426b87261a
Justin Haar <justin.haar22@gmail.com>
2015-03-24 14:50:29 -0700

    Initial Commit

---
+++ GetOnThatBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetOnThatBus
+//
+//  Created by Justin Haar on 3/24/15.
+//  Copyright (c) 2015 Justin Haar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
