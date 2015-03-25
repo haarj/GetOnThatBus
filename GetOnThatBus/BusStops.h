e9805eac75313095172d9cd2f9caf7abf6fd0fa5
Justin Haar <justin.haar22@gmail.com>
2015-03-24 23:55:20 -0700

    Initial Commit

---
+++ GetOnThatBus/BusStops.h
@@ -0,0 +1,13 @@
+//
+//  BusStops.h
+//  GetOnThatBus
+//
+//  Created by Justin Haar on 3/24/15.
+//  Copyright (c) 2015 Justin Haar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BusStops : NSObject
+
+@end
---
+++ GetOnThatBus/BusStops.m
@@ -0,0 +1,13 @@
+//
+//  BusStops.m
+//  GetOnThatBus
+//
+//  Created by Justin Haar on 3/24/15.
+//  Copyright (c) 2015 Justin Haar. All rights reserved.
+//
+
+#import "BusStops.h"
+
+@implementation BusStops
+
+@end
